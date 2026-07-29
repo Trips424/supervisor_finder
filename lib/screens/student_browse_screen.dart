fab31d87473659e07f2d58df21d637c01fac0882
Trips424 <uditini431@gmail.com>
2026-07-29 11:26:30 +0100

    created student browse screen

---
+++ lib/screens/student_browse_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../../models/staff_member.dart';
+import '../../widgets/staff_card.dart';
+
+class StudentBrowseScreen extends StatelessWidget {
+  const StudentBrowseScreen({
+    super.key,
+    required this.staffMembers,
+  });
