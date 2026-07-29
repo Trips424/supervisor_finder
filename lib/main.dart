b283ac4760381a14f5b56ab0bdc180ca39949cf6
Trips424 <uditini431@gmail.com>
2026-07-29 12:36:20 +0100

    add SupervisorFinderApp widget to initialize the app with a theme and home screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,16 @@ import 'screens/student_browse_screen.dart';
 void main() {
   runApp(const SupervisorFinderApp());
 }
+
+class SupervisorFinderApp extends StatelessWidget {
+  const SupervisorFinderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Supervisor Finder',
+      theme: ThemeData(primarySwatch: const Color.fromARGB(255, 9, 5, 228)),
+      home: const StudentBrowseScreen(staffMembers: sampleStaffMembers),
+    );
+  }
+}
