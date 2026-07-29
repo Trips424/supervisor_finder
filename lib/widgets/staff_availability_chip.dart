1112bf782405adf988bbec0a243917868c85690a
Trips424 <uditini431@gmail.com>
2026-07-29 20:29:27 +0100

    implemented and completed build method in StaffAvailabilityChip widget

--- lib/widgets/staff_availability_chip.dart
+++ lib/widgets/staff_availability_chip.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 class StaffAvailabilityChip extends StatelessWidget {
-  const StaffAvailabilityChip({
-    super.key,
-    required this.acceptingStudents,
-  });
+  const StaffAvailabilityChip({super.key, required this.acceptingStudents});
 
   final bool acceptingStudents;
 
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      label: Text(
+        acceptingStudents ? 'Accepting students' : 'Not accepting students',
+      ),
+      avatar: Icon(
+        acceptingStudents ? Icons.check_circle : Icons.cancel,
+        size: 18,
+      ),
+    );
+  }
+}
