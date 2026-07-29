92943c96022938d04fa4b1ac16bd3555fecd259e
Trips424 <uditini431@gmail.com>
2026-07-29 13:01:54 +0100

    add PageHeader widget for displaying a title and subtitle

---
+++ lib/widgets/page_header.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({super.key, required this.title, required this.subtitle});
+
+  final String title;
+  final String subtitle;
+}
