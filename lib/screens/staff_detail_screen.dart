f7a1cb303c895f34a5fca755bd49061c5c40e33c
Trips424 <uditini431@gmail.com>
2026-07-29 21:22:53 +0100

    implement build method in StaffDetailScreen widget

--- lib/screens/staff_detail_screen.dart
+++ lib/screens/staff_detail_screen.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 import '../../models/staff_member.dart';
 
 class StaffDetailScreen extends StatelessWidget {
-  const StaffDetailScreen({
-    super.key,
-    required this.staff,
-  });
+  const StaffDetailScreen({super.key, required this.staff});
 
   final StaffMember staff;
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(staff.name)),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Text(staff.biography, style: const TextStyle(fontSize: 16)),
+      ),
+    );
+  }
+}
