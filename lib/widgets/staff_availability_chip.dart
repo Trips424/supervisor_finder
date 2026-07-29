ed7812da340e0188021d374a532a8ab1b530d3dd
Trips424 <uditini431@gmail.com>
2026-07-29 20:27:20 +0100

    created StaffAvailabilityChip widget for displaying staff availability status

---
+++ lib/widgets/staff_availability_chip.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StaffAvailabilityChip extends StatelessWidget {
+  const StaffAvailabilityChip({
+    super.key,
+    required this.acceptingStudents,
+  });
+
+  final bool acceptingStudents;
+
