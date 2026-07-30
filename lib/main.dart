1f0a1da652b5e9d9b1a0a746d5bc898825ede7b2
Trips424 <uditini431@gmail.com>
2026-07-30 03:25:04 +0100

    refactor SupervisorFinderApp to use StatefulWidget for enhanced state management

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,15 @@ import 'package:flutter/material.dart';
 
 import 'data/staff_member_data.dart';
 import 'screens/student_browse_screen.dart';
+import 'screens/staff_dashboard_screen.dart';
 
 void main() {
   runApp(const SupervisorFinderApp());
 }
 
-class SupervisorFinderApp extends StatelessWidget {
+class SupervisorFinderApp extends StatefulWidget {
   const SupervisorFinderApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Supervisor Finder',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Supervisor Finder')),
-        body: StudentBrowseScreen(staffMembers: sampleStaffMembers),
-      ),
-    );
-  }
+  State<SupervisorFinderApp> createState() => _SupervisorFinderAppState();
 }
