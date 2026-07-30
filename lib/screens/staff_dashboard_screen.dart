05dbe0d0c82a24001821ba37e7fb2e63a88ce3ae
Trips424 <uditini431@gmail.com>
2026-07-30 05:43:58 +0100

    remove unused staffMembers parameter from StaffDashboardScreen

--- lib/screens/staff_dashboard_screen.dart
+++ lib/screens/staff_dashboard_screen.dart
@@ -6,7 +6,6 @@ import '../../models/staff_member.dart';
 class StaffDashboardScreen extends StatelessWidget {
   const StaffDashboardScreen({super.key, required this.selectedStaff});
 
-  final List<StaffMember> staffMembers;
   final StaffMember selectedStaff;
 
   @override
