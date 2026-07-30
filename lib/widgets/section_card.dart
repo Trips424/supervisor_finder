6b2c6bb802174b69ea6df03f21514de68e3158b5
Trips424 <uditini431@gmail.com>
2026-07-30 10:11:19 +0100

    created initial section card

---
+++ lib/widgets/section_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SectionCard extends StatelessWidget {
+  const SectionCard({
+    super.key,
+    required this.title,
+    required this.child,
+  });
+
+  final String title;
+  final Widget child;
+
